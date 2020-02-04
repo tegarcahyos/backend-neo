7f5ba4e4939ed6eaf8cb873b416c3fc82572fab3
Malik <malikabdulaziz51@gmail.com>
2020-02-04 16:46:07 +0700

    tes

--- models/program_charter.php
+++ models/program_charter.php
@@ -69,11 +69,11 @@ class ProgramCharter
                 CURLOPT_RETURNTRANSFER => true,
                 CURLOPT_TIMEOUT => 30,
                 CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => "GET"
+                CURLOPT_CUSTOMREQUEST => "GET",
             ));
 
+        die(print_r($curl));
         $response = curl_exec($curl);
-        die(print_r($response));
         $err = curl_error($curl);
 
         curl_close($curl);
