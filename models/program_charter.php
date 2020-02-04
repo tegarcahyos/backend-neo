8fb862dec5db8f52da7a029f4d8324a9ad82e057
Malik <malikabdulaziz51@gmail.com>
2020-02-04 16:53:59 +0700

    tes

--- models/program_charter.php
+++ models/program_charter.php
@@ -104,7 +104,7 @@ class ProgramCharter
     {
         $getData = $this->callAPI('GET', '10.62.161.11/api/index.php/program_charter/get', false);
         $request = json_decode($getData);
-        die(print_r($request));
+        die(print_r($request->data));
     }
 
     public function findById($id, $tablename)
