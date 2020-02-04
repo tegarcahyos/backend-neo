b32781ad9b798d8c0c3794e6155590d83e5f22ae
Malik <malikabdulaziz51@gmail.com>
2020-02-04 16:45:43 +0700

    tes

--- models/program_charter.php
+++ models/program_charter.php
@@ -69,10 +69,7 @@ class ProgramCharter
                 CURLOPT_RETURNTRANSFER => true,
                 CURLOPT_TIMEOUT => 30,
                 CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => "GET",
-                CURLOPT_HTTPHEADER => array(
-                    "cache-control: no-cache",
-                ),
+                CURLOPT_CUSTOMREQUEST => "GET"
             ));
 
         $response = curl_exec($curl);
