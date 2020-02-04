7b5e3178716a91cf269564a6bd3a88d466a4817a
Malik <malikabdulaziz51@gmail.com>
2020-02-04 16:56:36 +0700

    tes

--- models/program_charter.php
+++ models/program_charter.php
@@ -104,7 +104,7 @@ class ProgramCharter
     {
         $getData = $this->callAPI('GET', '10.62.161.11/api/index.php/program_charter/get', false);
         $request = json_decode($getData);
-        $status_decode = json_decode($request->data->status);
+        $status_decode = json_decode($request->data['status']);
         die(print_r($status_decode));
     }
 
