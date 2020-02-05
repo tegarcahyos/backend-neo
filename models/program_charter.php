dead8556e6b0ede5bc32c418ea66ca92969673fd
Malik <malikabdulaziz51@gmail.com>
2020-02-05 14:01:49 +0700

    tes

--- models/program_charter.php
+++ models/program_charter.php
@@ -59,60 +59,6 @@ class ProgramCharter
         return $msg;
     }
 
-    public function callAPI($method, $url, $data)
-    {
-        $curl = curl_init();
-        switch ($method) {
-            case "POST":
-                curl_setopt($curl, CURLOPT_POST, 1);
-                if ($data) {
-                    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-                }
-
-                break;
-            case "PUT":
-                curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
-                if ($data) {
-                    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-                }
-
-                break;
-            default:
-                if ($data) {
-                    $url = sprintf("%s?%s", $url, http_build_query($data));
-                }
-
-        }
-
-        // OPTIONS:
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-            'Content-Type: application/json',
-            // 'x-authorization: ' . $this->apiKey,
-        ));
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-        // EXECUTE:
-        $result = curl_exec($curl);
-        // die("ini token" . $this->apiKey);
-        if (!$result) {die("Connection Failure");}
-        curl_close($curl);
-        return $result;
-    }
-
-    public function getAcceptedPC()
-    {
-        $getData = $this->callAPI('GET', '10.62.161.11/api/index.php/program_charter/get', false);
-        $request = json_decode($getData);
-        $status_decode = json_decode($request->data[0]->status);
-        // die(print_r($status_decode->status));
-        if ($status_decode->status == 'accepted') {
-            return $request;
-        } else {
-            return "Data Kosong";
-        }
-    }
-
     public function findById($id, $tablename)
     {
         $query = "SELECT * FROM $tablename WHERE id = '$id'";
